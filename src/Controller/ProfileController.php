73b3733a19c75723b3251a0841f1521d6aebc473
rahdy <rahdy.issouf@hotmail.fr>
2022-11-10 10:22:58 +0100

    Initial commit

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/profil', name: 'profile_')]
+class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(UsersRepository $usersRepository): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'users' => $usersRepository->findBy([], ['id' => 'asc'])
+        ]);
+    }
+
+    #[Route('/commandes', name: 'orders')]
+    public function orders(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'Commandes de l\'utilisateur',
+        ]);
+    }
+}
